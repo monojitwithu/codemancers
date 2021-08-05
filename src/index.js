7055dd2e60b7eda787acd207033929da67ed3930
Monojit Das <monojitwithu@gmail.com>
2021-08-05 12:51:48 +0530

    FolderStructure Created

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import main from './styles/main.scss'
+
+
+ReactDOM.render(
+  
+    <App />
+  ,
+  document.getElementById('root')
+);
+
+
